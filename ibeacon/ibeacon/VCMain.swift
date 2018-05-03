bd2256ddff0b9a52995e11ea8b89d278f99cd8da
Surattikorn Chumkaew <surattikorn@gmail.com>
2018-05-03 11:36:01 +0700

    init project

---
+++ ibeacon/ibeacon/VCMain.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ibeacon
+//
+//  Created by Surattikorn Chumkaew on 3/5/2561 BE.
+//  Copyright © 2561 example. All rights reserved.
+//
+
+import UIKit
+
+class VCMain: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
